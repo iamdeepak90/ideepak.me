b9f20df3fa9d656c73fe008b217bca94b0d00a0e
Deepak Mishra <deepak@wisitech.com>
2023-05-19 05:26:46 +0530

    Fix the build issue

---
+++ .next/static/WMemEEq8EyIDSxSr8VGbN/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/300-9d3ecee1d44a0782.js","static/chunks/pages/index-9fefdb8df9a4535b.js"],"/_error":["static/chunks/pages/_error-8022dacb1937fc7a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
