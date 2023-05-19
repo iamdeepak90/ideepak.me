7ce4fe474d9e8bcde16406d8ecf22dff95f6a043
Deepak Mishra <deepak@wisitech.com>
2023-05-19 05:57:55 +0530

    Next Update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -150,7 +150,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b1e20b3e971b3b99"; }
+/******/ 		__webpack_require__.h = function() { return "f6c3cb5b4e760dfb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
