2f6617de9a3611a348b3fa4dea0586fcd19cfe85
Deepak Mishra <deepak@wisitech.com>
2023-05-22 03:17:07 +0530

    Update mobile header color

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -150,7 +150,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d074dbc8c4dc05a2"; }
+/******/ 		__webpack_require__.h = function() { return "77298c5e65234433"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
