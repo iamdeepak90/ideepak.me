13e06eee0cb936094254bd2021f54a6c53afbffc
Deepak Mishra <deepak@wisitech.com>
2023-05-23 01:46:39 +0530

    Update Home Text

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -150,7 +150,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "90575b521678a71e"; }
+/******/ 		__webpack_require__.h = function() { return "db386790314064d3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/Home.js
+++ src/components/Home.js
@@ -23,16 +23,16 @@ const Home = ({ dark }) => {
                 <h3 className="orangeText">{`Hello, I'm`}</h3>
               </div>
               <div className="name">
-                <h3>{data && data.name ? data.name : "name"}</h3>
+                <h3>DEEPAK MISHRA</h3>
               </div>
               <div className="job">
                 <p>
-                  A <span className="greenText">{data && data.mainSkill}</span>{" "}
-                  From <span className="purpleText">{data.address}</span>
+                  A <span className="greenText">Full Stack Developer </span>
+                  From <span className="purpleText">New Delhi</span>
                 </p>
               </div>
               <div className="text">
-                <p>{data.bio}</p>
+                <p>Full Stack Web Developer proficient in front-end and back-end development, creating robust and user-friendly web solutions.</p>
               </div>
               <div className="button">
                 <div className="dizme_tm_button">
